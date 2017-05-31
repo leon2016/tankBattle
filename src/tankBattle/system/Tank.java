25868985ae7660dc6452741cbb6e9d69be145e5d
leon <1754235453@qq.com>
2017-06-01 00:48:54 +0800

    测试push

--- src/tankBattle/system/Tank.java
+++ src/tankBattle/system/Tank.java
@@ -4,7 +4,5 @@ public class Tank {
     public static void main(String[] args) {
         System.out.println("hello git!");
     }
-  public static void main(String[] args) {
-        System.out.println("hello world!");
-    }
+
 }
